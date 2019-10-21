a71a0338f03d1c0d70f782e0f893973b4525b00e
spencercv7 <16scpv@queensu.ca>
2019-10-20 23:47:01 -0400

    Updates

---
+++ precache-manifest.c85a1ca3399f7ffd0a4b3a149d1ea196.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4553a22d2898e167bb12bab08cecc05d",
+    "url": "/spencer_web/index.html"
+  },
+  {
+    "revision": "ffebdcec2cd34b41a7a9",
+    "url": "/spencer_web/static/css/main.1d2d2897.chunk.css"
+  },
+  {
+    "revision": "b9e36d7ac15b3a5b23d6",
+    "url": "/spencer_web/static/js/2.4a1df924.chunk.js"
+  },
+  {
+    "revision": "ffebdcec2cd34b41a7a9",
+    "url": "/spencer_web/static/js/main.53f243d4.chunk.js"
+  },
+  {
+    "revision": "bcb910ba11d5949d9d7b",
+    "url": "/spencer_web/static/js/runtime-main.2f0bd14e.js"
+  }
+]);
