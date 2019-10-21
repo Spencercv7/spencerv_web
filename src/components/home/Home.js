3f4a0bc2b04b4a06ca462e89853cd40a99b5c4b5
spenc <16scpv@queensu.ca>
2019-10-21 13:53:05 -0400

    Concept Check

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,15 +1,21 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 import './home_style.css'
-import Home_Box from './home_components/Home_Box'
+import P5Wrapper from 'react-p5-wrapper';
+import sketch from './js/Home_Back_Sketch'
 
 class Home extends React.Component {
+      
+      
       render() {
             return (
               <div id="home_container">
-                  <div id="home_deco_box"></div>
-                  <Home_Box/>
+                  <div class= "header_title">
+                        <h2>SPENCER VENABLE.</h2>
+                        <h6>Software Developer</h6>
+                  </div>
                   <nav class="main_nav"></nav>
+                  <P5Wrapper id="home_background_animation" sketch={sketch}></P5Wrapper>
               </div>   
             );
       }
